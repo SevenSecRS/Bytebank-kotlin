5cb41c2fcd9e8dd37038e40b5394e49c08339c23
Vitor Eleoterio <vitor.eleoterio@ifood.com.br>
2020-10-21 13:24:11 -0300

    pacotes e composição

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+fun main() {
+
+  print("Digite um numero: ")
+    var numeros = readLine()?.toInt() ?: 0
+
+    if (numeros = %0) {
+        println("O numero $numeros é PAR")
+    }else{
+        print("O numero $numeros é IMPAR")
+    }
+
+}
+
+
+
+
+
+
+
+
